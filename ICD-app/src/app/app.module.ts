be6c352f332bc76adc29f086fc2c47bee26a1052
Malin Dulkies <kay.sian@gmx.de>
2021-11-03 13:30:51 +0100

    Application Basics

---
+++ ICD-app/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LandingpageComponent } from './landingpage/landingpage.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LandingpageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
